fbd2fcce760f13a30bff6d26d35be48b249f5eb4
LucasSlv <lucassilvax005x@gmail.com>
2022-08-17 21:46:33 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initTyping from "./utils/typing.js";
+import initMenuMobile from "./utils/menu-mobile.js";
+import initTabNav from "./utils/tabnav.js"
+
+initTyping();
+initMenuMobile();
+initTabNav();
